d29bc8a65bfd9597ae8cfe5c040a4fd863131865
aarifsk <aarifsk1618@gmail.com>
2021-12-04 00:51:43 +0900

    add props to comp

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,16 +1,16 @@
 import React from "react"
 
-const ItemCard = () => (
+const ItemCard = ({item}) => (
     <div className="ui card">
         <div className="image">
             <span className="ui green ribbon label">
-                $32.99</span><img src="https://picsum.photos/200/300" alt="randomimage" />
+                {item.price}</span><img src={item.image} alt="randomimage" />
         </div>
         <div className="content">
-            <a href="#" className="header">Sample 1</a>
+            <a href="#" className="header">{item.name}</a>
             <div className="meta">
-                <i className="icon users"/>2-4&nbsp;
-                <i className="icon wait"/>50min
+                <i className="icon users"/>{item.licenses}&nbsp;
+                <i className="icon wait"/>{item.duration}min.
             </div>
         </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import {render} from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import ItemCard from './components/ItemCard';
 
+const items = [
+  {"name": "Software 1", "price":"32.99", "licenses":"4", "duration":"60", "image":"https://picsum.photos/200/300"},
+  {"name": "Software 2", "price":"3.9", "licenses":"3", "duration":"30", "image":"https://picsum.photos/200/300"},
+  {"name": "Software 3", "price":"49.2", "licenses":"1", "duration":"180", "image":"https://picsum.photos/200/300"}
+]
 render(
   <React.StrictMode>
-    <ItemCard />
+    <ItemCard item={items[0]} />
   </React.StrictMode>,
   document.getElementById('root')
 );
